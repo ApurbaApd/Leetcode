4fd6dccb8ac6588a865828ab45dc6a548e27a29e
Apurba Debnath <119648597+ApurbaApd@users.noreply.github.com>
2023-06-09 16:49:11 +0530

    Added solution - LeetHub

---
+++ Number of distinct subsequences - GFG/number-of-distinct-subsequences.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int distinctSubsequences(string s) {
+        int n = s.size();
+        vector<long long> dp(n + 1, 0);
+        vector<int> lastOccur(26, -1);  // Stores the last occurrence index of each character
+
+        dp[0] = 1;  // Empty string has 1 distinct subsequence
+
+        const int MOD = 1e9 + 7;
+        
+        for (int i = 1; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1]) % MOD;
+            
+            if (lastOccur[s[i - 1] - 'a'] != -1) {
+                dp[i] = (dp[i] - dp[lastOccur[s[i - 1] - 'a']]) % MOD;
+            }
+            
+            lastOccur[s[i - 1] - 'a'] = i - 1;
+        }
+        
+        if (dp[n] < 0) {
+            dp[n] += MOD;
+        }
+        
+        return dp[n];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+        Solution ob;
+   		cout << ob.distinctSubsequences(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
