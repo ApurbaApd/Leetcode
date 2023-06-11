065f6301937fceeaebcb46b8aaaaecf91202b6f1
Apurba Debnath <119648597+ApurbaApd@users.noreply.github.com>
2023-06-11 08:46:38 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -8,17 +8,27 @@ using namespace std;
 class Solution {
 public:
     int longestSubsequence(int n, int a[]) {
+        // Create an empty vector to store the increasing subsequence elements
         vector<int> dp;
         
+        // Iterate over each element in the input array
         for(int i = 0; i < n; i++) {
+            // Perform binary search to find the position where a[i] can be inserted
             auto it = lower_bound(dp.begin(), dp.end(), a[i]);
+            
+            // If the binary search returns dp.end(), a[i] is greater than all elements
+            // in the dp array, so append a[i] to the end of the dp array
             if(it == dp.end()) {
                 dp.push_back(a[i]);
-            } else {
+            } 
+            // If the binary search returns an iterator pointing to an element in dp,
+            // replace that element with a[i] to maintain the sorted order
+            else {
                 *it = a[i];
             }
         }
         
+        // Return the size of the dp array, which represents the length of the longest increasing subsequence
         return dp.size();
     }
 };
