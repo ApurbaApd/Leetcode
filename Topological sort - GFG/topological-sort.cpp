b7cdc1e288eadf1bbdfd46e0a44f34a42ca3f273
Apurba Debnath <119648597+ApurbaApd@users.noreply.github.com>
2023-06-17 12:54:26 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -4,40 +4,88 @@ using namespace std;
 
 // } Driver Code Ends
 
-class Solution
-  ///Will use DFS, TC-O(V+E), SC-O(N)
-{   private:
-       void dfs(int node,int vis[], stack<int>&st,vector<int> adj[] ){
-           vis[node] = 1;
-           for(auto it : adj[node]){
-               if(!vis[it]){
-                   dfs(it, vis, st, adj);
-               }
-           }
-           st.push(node);
-       }
-	public:
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    // code here
-	    int vis[V] = {0};
-	    stack<int>st;
-	    
-	    for(int i=0;i<V;i++){
-	        if(!vis[i]){
-	            dfs(i, vis, st, adj);
-	        }
-	    }
-	    
-	    vector<int>ans;
-	    while(!st.empty()){
-	        ans.push_back(st.top());
-	        st.pop();
-	    }
-	    return ans;
-	}
+
+class Solution{
+    //Topo Sort using BFS, Kahan's Algorithm
+    public:
+    vector<int> topoSort(int V, vector<int> adj[]){
+        
+        vector<int> indegree(V, 0);
+        for (int i = 0; i < V; i++) {
+            for (auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        vector<int> topo;
+
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for (auto it : adj[node]) { // Neighbors of node
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        return topo;
+    }
+    
 };
+
+
+
+
+
+
+
+
+
+// class Solution
+//   ///Will use DFS, TC-O(V+E), SC-O(N)
+// {   private:
+//       void dfs(int node,int vis[], stack<int>&st,vector<int> adj[] ){
+//           vis[node] = 1;
+//           for(auto it : adj[node]){
+//               if(!vis[it]){
+//                   dfs(it, vis, st, adj);
+//               }
+//           }
+//           st.push(node);
+//       }
+// 	public:
+// 	//Function to return list containing vertices in Topological order. 
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    // code here
+// 	    int vis[V] = {0};
+// 	    stack<int>st;
+	    
+// 	    for(int i=0;i<V;i++){
+// 	        if(!vis[i]){
+// 	            dfs(i, vis, st, adj);
+// 	        }
+// 	    }
+	    
+// 	    vector<int>ans;
+// 	    while(!st.empty()){
+// 	        ans.push_back(st.top());
+// 	        st.pop();
+// 	    }
+// 	    return ans;
+// 	}
+// };
 
 
 
