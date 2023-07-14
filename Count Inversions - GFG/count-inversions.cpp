79d09c13156b08275a1f052af330d2700a71fffb
Apurba Debnath <119648597+ApurbaApd@users.noreply.github.com>
2023-07-14 09:30:18 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -4,67 +4,63 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution {
+
+class Solution{
+ private:
+    void merge(long long arr[], long long l, long long m, long long h,long long &ans)
+    {
+         vector<long long> temp;
+         long long left=l,right=m+1;
+         while(left<=m && right<=h){
+             if(arr[left]<=arr[right]){
+                 temp.push_back(arr[left]);
+                 left++;
+             }
+             else{
+                 temp.push_back(arr[right]);
+                 right++;
+                 ans+=(m-left+1);
+             }
+         }
+         while(left<=m){
+              temp.push_back(arr[left]);
+                 left++;
+         }
+         while(right<=h){
+               temp.push_back(arr[right]);
+                 right++;
+          }
+        //  long long n=temp.size();
+        //  for(long long i=0;i<n;i++){
+        //      arr[i+l]=temp[i];
+        //  }
+        
+        for(long long i = l; i<=h; i++){
+            arr[i] = temp[i-l];
+        }
+    }
+    void mergeSort(long long arr[], long long l, long long h,long long &ans)
+    {
+        if(l==h) return;
+        long long m=(l+h)/2;
+        mergeSort(arr,l,m,ans);
+        mergeSort(arr,m+1,h,ans);
+        merge(arr,l,m,h,ans);
+    }
 public:
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
     // Function to count inversions in the array.
-    long long int inversionCount(long long arr[], long long N) {
-        // Create a temporary array for merging
-        long long* temp = new long long[N];
-        return mergeSort(arr, temp, 0, N - 1);
+    long long int inversionCount(long long arr[], long long N)
+    {
+        long long ans=0;
+        mergeSort(arr,0,N-1,ans);
+        return ans;
     }
 
-private:
-    long long int merge(long long arr[], long long temp[], long long left, long long mid, long long right) {
-        long long i = left;    // Index for left subarray
-        long long j = mid + 1; // Index for right subarray
-        long long k = left;    // Index for merged subarray
-        long long inversionCount = 0;
-
-        // Merge the two subarrays while counting inversions
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                temp[k++] = arr[i++];
-            } else {
-                temp[k++] = arr[j++];
-                inversionCount += (mid - i + 1); // Count inversions
-            }
-        }
-
-        // Copy the remaining elements of left subarray (if any)
-        while (i <= mid) {
-            temp[k++] = arr[i++];
-        }
-
-        // Copy the remaining elements of right subarray (if any)
-        while (j <= right) {
-            temp[k++] = arr[j++];
-        }
-
-        // Copy the merged elements back to the original array
-        for (i = left; i <= right; i++) {
-            arr[i] = temp[i];
-        }
-
-        return inversionCount;
-    }
-
-    long long int mergeSort(long long arr[], long long temp[], long long left, long long right) {
-        long long inversionCount = 0;
-
-        if (left < right) {
-            long long mid = (left + right) / 2;
-
-            // Recursively count inversions in the left and right subarrays
-            inversionCount += mergeSort(arr, temp, left, mid);
-            inversionCount += mergeSort(arr, temp, mid + 1, right);
-
-            // Merge the two sorted subarrays and count inversions
-            inversionCount += merge(arr, temp, left, mid, right);
-        }
-
-        return inversionCount;
-    }
 };
+
+
 
 
 //{ Driver Code Starts.
